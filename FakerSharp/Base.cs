75c07812c7e99b8d4844b9e26222efd1024ba15f
Rodolfo Carvalho <rodolfo.ueg@gmail.com>
2011-05-09 08:31:58 -0300

    Initial Commit

---
+++ FakerSharp/Base.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace InfWeb.FakerSharp
+{
+	public abstract class Base
+	{
+		private static readonly char[] LETTERS = "abcdefghijklmnopqrstuwxyz".ToArray();
+
+		private static Random random = new Random();
+		protected static string Numerify(string numberString)
+		{
+			var regex = new Regex(@"#");
+			return regex.Replace(numberString, (match) => random.Next(10).ToString());
+		}
+
+		protected static string Letterify(string letterString)
+		{
+			var regex = new Regex(@"\?");
+			return regex.Replace(letterString, (match) => LETTERS[random.Next(LETTERS.Length)].ToString());
+		}
+
+		protected static string Bothify(string str)
+		{
+			return Letterify(Numerify(str));
+		}
+	}
+}
---
+++ FakerSharp/IEnumerableExtension.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace InfWeb.FakerSharp
+{
+	public static class IEnumerableExtension
+	{
+		public static object Pick(this IEnumerable collection)
+		{
+			return Pick<object>(collection.Cast<object>());
+		}
+
+		public static TResult Pick<TResult>(this IEnumerable<TResult> collection)
+		{
+			int idx = new Random().Next(collection.Count());
+			return collection.ElementAt(idx);
+		}
+	}
+}
